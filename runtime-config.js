aadc3b6661b233592d33d14b30807ba463228457
Tien Tran <xtien1812@gmail.com>
2026-06-10 09:25:18 +0700

    chore: enable debug mode in runtime configuration

--- runtime-config.js
+++ runtime-config.js
@@ -2,5 +2,5 @@ window.__TREVO_RUNTIME_CONFIG__ = {
   "orgSlug": "tea-station",
   "apiBaseUrl": "https://api.trevo.studio",
   "frontendBaseUrl": "https://app.trevo.studio",
-  "debug": false
+  "debug": true
 };
